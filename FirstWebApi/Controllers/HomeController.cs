7de37ad5d74b809a366ddd90185f16ad0eb54a03
demirtasenes4 <demirtasenes2@gmail.com>
2023-11-15 20:34:40 +0300

    Add project files.

---
+++ FirstWebApi/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers;
+[ApiController]
+[Route("api/[controller]/[action]")]
+public sealed class HomeController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        Test test = new();
+        test.Age = 1;
+        test.Name = "Enes";
+        return NoContent();
+    }
+
+    [HttpGet]
+    public string Get2()
+    {
+        return "Enes Demirtas";
+    }
+}
+
+public class Test
+{
+    public string Name { get; set; }
+    public int Age;
+}
